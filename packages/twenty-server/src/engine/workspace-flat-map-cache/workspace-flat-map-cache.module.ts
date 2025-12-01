34f3b88e779863052efa81749999a5d37ff95897
prastoin <paul@twenty.com>
2025-12-01 10:47:58 +0100

    feat(server): flat agent cache service

--- packages/twenty-server/src/engine/workspace-flat-map-cache/workspace-flat-map-cache.module.ts
+++ packages/twenty-server/src/engine/workspace-flat-map-cache/workspace-flat-map-cache.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { DiscoveryModule } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AgentEntity } from 'src/engine/metadata-modules/ai/ai-agent/entities/agent.entity';
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
+import { WorkspaceFlatAgentMapCacheService } from 'src/engine/metadata-modules/flat-agent/services/workspace-flat-agent-map-cache.service';
 import { WorkspaceFlatFieldMetadataMapCacheService } from 'src/engine/metadata-modules/flat-field-metadata/services/workspace-flat-field-metadata-map-cache.service';
 import { WorkspaceFlatIndexMapCacheService } from 'src/engine/metadata-modules/flat-index-metadata/services/workspace-flat-index-map-cache.service';
 import { WorkspaceFlatObjectMetadataMapCacheService } from 'src/engine/metadata-modules/flat-object-metadata/services/workspace-flat-object-metadata-map-cache.service';
@@ -33,6 +35,7 @@ import { WorkspaceFlatMapCacheRegistryService } from 'src/engine/workspace-flat-
       IndexFieldMetadataEntity,
       FieldMetadataEntity,
       ObjectMetadataEntity,
+      AgentEntity,
     ]),
   ],
   providers: [
@@ -44,6 +47,7 @@ import { WorkspaceFlatMapCacheRegistryService } from 'src/engine/workspace-flat-
     WorkspaceFlatIndexMapCacheService,
     WorkspaceFlatFieldMetadataMapCacheService,
     WorkspaceFlatViewGroupMapCacheService,
+    WorkspaceFlatAgentMapCacheService,
   ],
   exports: [
     WorkspaceFlatMapCacheRegistryService,
@@ -54,6 +58,7 @@ import { WorkspaceFlatMapCacheRegistryService } from 'src/engine/workspace-flat-
     WorkspaceFlatIndexMapCacheService,
     WorkspaceFlatFieldMetadataMapCacheService,
     WorkspaceFlatViewGroupMapCacheService,
+    WorkspaceFlatAgentMapCacheService,
   ],
 })
 export class WorkspaceFlatMapCacheModule {}
